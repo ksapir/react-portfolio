1e17d46787405d606ed22d4503925b1aadd12c0a
Karen Pion <karenlpion@gmail.com>
2021-08-25 17:12:08 -0400

    highlighting pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
 import React from "react";
-import Header from "./components/Header";
+// import Header from "./components/Header";
 import AboutMe from "./components/AboutMe";
 import MyWork from "./components/MyWork";
 import ContactMe from "./components/ContactMe";
 import Resume from "./components/Resume";
 import Footer from "./components/Footer";
-import { Route, Link } from "react-router-dom";
 import './styles/script.css'
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch
+} from "react-router-dom";
+
+const styles = {
+  nav: {
+      marginRight: '20px'
+  },
+  a: {
+      marginRight: '5px',
+      marginLeft: '5px'
+  },
+  header: {
+    height: '200px'
+  },
+
+}
 
 function App() {
     return (
+      <Router>
+      <div>
+          <header style={styles.header}>
+              <h1>Karen Pion</h1>
+              <nav style={styles.nav}>
+                  <OldSchoolMenuLink style={styles.a} activeOnlyWhenExact={true} to='/' label="About Me"></OldSchoolMenuLink>
+                  <OldSchoolMenuLink style={styles.a} to='/mywork' label="My Work"></OldSchoolMenuLink>
+                  <OldSchoolMenuLink style={styles.a} to='/contact'label="Contact Me"></OldSchoolMenuLink>
+                  <OldSchoolMenuLink style={styles.a} to='/resume'label="Resume"></OldSchoolMenuLink>
+              </nav>
+          </header>
+      </div>
         <div className="App">
-          <Header />
-          <Route exact path="/" component={AboutMe} />
-          <Route exact path="/mywork" component={MyWork} />
-          <Route exact path="/contact" component={ContactMe} />
-          <Route exact path="/resume" component={Resume} />
+          <Switch>
+          <Route exact path="/"><AboutMe /></Route>
+          <Route exact path="/mywork"> <MyWork /></Route>
+          <Route exact path="/contact"><ContactMe /></Route>
+          <Route exact path="/resume"><Resume/></Route>
+          </Switch>
           <Footer />
         </div>
+      </Router>
       );
 }
 
-export default App;
+function OldSchoolMenuLink({ label, to, activeOnlyWhenExact }) {
+  let match = useRouteMatch({
+    path: to,
+    exact: activeOnlyWhenExact
+  });
+
+  return (
+    <div className={match ? "active" : ""}>
+      {match && "> "}
+      <Link to={to}>{label}</Link>
+    </div>
+  );
+}
+
+export default App
--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -45,12 +45,10 @@ export default function ContactMe() {
     const [errorMessage, setErrorMessage] = useState('');
 
     const handleInputChange = (e) => {
-        // Getting the value and name of the input which triggered the change
         const { target } = e;
         const inputType = target.name;
         const inputValue = target.value;
 
-        // Based on the input type, we set the state of either email, username, and password
         if (inputType === 'email') {
             setEmail(inputValue);
         } else if (inputType === 'name') {
@@ -61,18 +59,12 @@ export default function ContactMe() {
     };
 
     const handleFormSubmit = (e) => {
-        // Preventing the default behavior of the form submit (which is to refresh the page)
         e.preventDefault();
 
-        // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
         if (!validateEmail(email)) {
             setErrorMessage('Please enter a valid email');
-            // We want to exit out of this code block if something is wrong so that the user can correct it
             return;
-            // Then we check to see if the password is not valid. If so, we set an error message regarding the password.
         }
-
-        // If everything goes according to plan, we want to clear out the input after a successful registration.
         setName('');
         setMessage('');
         setEmail('');
